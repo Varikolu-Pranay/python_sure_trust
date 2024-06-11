08b9627864ecfa330bda337a8b61820a4e55f4f7
Varikolu-Pranay <pranayofficial13@gmail.com>
2024-06-11 20:58:44 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -4,3 +4,11 @@ print(new1[len(new1)-1])
 print(new1[-3])
 print(new1.index("de"))
 print(new1.count("da"))
+
+x,y,z=1,2,3  # if there is comma's in the code it understands as tupple
+co_ordinates=y,x,y,x,z
+
+x=5   # x value will not be chnaged 
+print(x)
+
+print(co_ordinates)
