0633bd48b90e2be0fa47e1f7396f1680a4732551
Varikolu-Pranay <pranayofficial13@gmail.com>
2024-06-12 20:56:44 +0530

    12-06-24

---
+++ list_concept.py
@@ -0,0 +1,55 @@
+# x=(10)
+# print(x,type(x)) # it consider it as integer
+# x=(10,)
+# print(x,type(x)) # it consider as tuple
+
+#----------------list-----------------------# 
+
+# x=["hi","my","name","is","..."]
+
+# x.append("hello")
+# print(x)
+# y= x.pop() # the popped can bestores in a variable
+# print(x)
+# print(y)
+
+# x.insert(4,"pranay")  # insert a value in a particular index /the inderx specifies the position of the added element
+# print(x)
+
+# x.insert(-1,"kumar") # len(x)-1 :the way it converts into positive using the formula
+# print(x)
+
+# y=x.pop()
+# print(x)
+# x.pop(2)
+# print(x)
+
+# x.remove("is")
+# print(x)
+# x.pop(x.index("hi")) # internal part od remove function
+
+# x[-1]="varikolu" # it adds the element by deleteing that
+# print(x)
+
+# sentence ="__".join(x) # to convert from list to string
+# print(sentence)
+
+# print(sentence.split("__")) # string to a list
+
+# sentence =list("Hello")
+# print(" ".join(sentence))
+# x=[1,2,3,4,5]
+# sentence =str(x) # it considers as the  "[1,2,3,4,5]"
+# print(sentence,type(sentence)) 
+# print(sentence.split(","))
+
+x=input("enter 3 names : ")
+x=x.split(",")
+
+x[-2]="changed_name"
+y=x.pop()
+x.insert(1,y)
+x.append("new_name")
+x.pop(0)
+
+print(",".join(x))
--- tuples.py
+++ tuples.py
@@ -8,7 +8,13 @@ print(new1.count("da"))
 x,y,z=1,2,3  # if there is comma's in the code it understands as tupple
 co_ordinates=y,x,y,x,z
 
-x=5   # x value will not be chnaged 
+x=5   # x value will not be chnaged in the tuple
 print(x)
 
 print(co_ordinates)
+print(sum(co_ordinates)) # only numerical tuples can be added not strings
+
+new2 =("su","mn",True,("sa","kl",(3,4,1),"jl"),"tue",1,"wed")
+print(new2[3])
+print(new2[-4][1])
+print(new2[-4] [2][1])
